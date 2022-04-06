567d61d899c01caa371f2f89e1065d78a414d583
Annsh Agrawaal <aagraw26@uic.edu>
2022-04-06 12:29:25 -0500

    Update 49. Group Anagrams.java

--- 49. Group Anagrams.java
+++ 49. Group Anagrams.java
@@ -1,3 +1,5 @@
+
+// Sorting - O(nlog(n))
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> hmap = new HashMap<>();
@@ -13,5 +15,27 @@ class Solution {
         }
         
         return new ArrayList<>(hmap.values());
+    }
+}
+
+
+// Hashing Array - O(n)
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap <String,List<String>> hmap = new HashMap<>();
+        for(String s: strs){
+            char[] charr = new char[26];
+            for(char c: s.toCharArray()){
+                charr[c-'a']++;
+            }
+            String str = String.valueOf(charr);
+            if(!hmap.containsKey(str)){
+                hmap.put(str,new ArrayList<>());
+            }
+            hmap.get(str).add(s);
+        }
+
+        return new ArrayList<>(hmap.values()); 
+
     }
 }
